d9d2cf752a6cfa13c7c100b45ed6a7ca9467ff84
omarhmdea <omarhm@ac.sce.ac.il>
2022-12-15 19:25:35 +0200

    validate register and login data done

--- src/main/java/calendar/exception/ControllerAdvisor.java
+++ src/main/java/calendar/exception/ControllerAdvisor.java
@@ -3,6 +3,7 @@ package calendar.exception;
 import calendar.ResponsHandler.ErrorResponse;
 import calendar.ResponsHandler.SuccessResponse;
 import calendar.entities.LoginData;
+import calendar.exception.customException.ValidationErrorException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,12 +11,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    public ResponseEntity<ErrorResponse<String>> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
+        ErrorResponse<String> errorResponse = new ErrorResponse<>(HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(ValidationErrorException.class)
+    public ResponseEntity<ErrorResponse<Map<String, String>>> handleValidationErrorException(ValidationErrorException ex, WebRequest request) {
+        Map<String, String> errors = ex.getErrors();
+        ErrorResponse<Map<String, String>> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST, errors);
         return ResponseEntity.badRequest().body(errorResponse);
     }
 }
---
+++ src/main/java/calendar/utilities/Validator.java
@@ -0,0 +1,137 @@
+package calendar.utilities;
+
+
+import calendar.entities.User;
+import calendar.entities.UserCredentials;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    private static final Map<String, String> errorsMap = new HashMap<String, String>();
+    private static final Logger logger = LogManager.getLogger(Validator.class.getName());
+
+
+    public static Optional<Map<String, String>> validateRegister(User user) {
+        errorsMap.clear();
+
+        if(! isValidName(user.getName())) {
+            errorsMap.put("name", "name" + getNameConstraints());
+        }
+        if(! isValidEmail(user.getEmail())) {
+            errorsMap.put("email", "invalid email");
+        }
+        if(! isValidPassword(user.getPassword())) {
+            errorsMap.put("password", getPasswordConstraints());
+        }
+        if(errorsMap.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(errorsMap);
+    }
+
+
+    public static Optional<Map<String, String>> validateLogin(UserCredentials user) {
+        errorsMap.clear();
+
+        if(! isValidEmail(user.getEmail())) {
+            errorsMap.put("email", "invalid email");
+        }
+        if(! isValidPassword(user.getPassword())) {
+            errorsMap.put("password", getPasswordConstraints());
+        }
+        if(errorsMap.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(errorsMap);
+    }
+
+
+//    public static Optional<Map<String, String>> validateFields(Map<String, String> fields) {
+//        errorsMap.clear();
+//
+//        fields.forEach((key, value) -> {
+//            switch(key) {
+//                case "firstName":
+//                    if(! isValidName(value)) {
+//                        errorsMap.put("firstName", "first name" + getNameConstraints());
+//                    }
+//                    break;
+//                case "lastName":
+//                    if(! isValidName(value)) {
+//                        errorsMap.put("lastName", "last name" + getNameConstraints());
+//                    }
+//                    break;
+//
+//                case "email":
+//                    if(! isValidEmail(value)) {
+//                        errorsMap.put("email", "invalid email");
+//                    }
+//                    break;
+//
+//                case "nikeName":
+//                    if(! isValidNiceName(value)) {
+//                        errorsMap.put("nikeName", getNikeNameConstraints());
+//                    }
+//                    break;
+//            }
+//        });
+//
+//        if(errorsMap.isEmpty()) {
+//            return Optional.empty();
+//        }
+//        return Optional.of(errorsMap);
+//    }
+    /**
+     * Is valid name : check if The length of the name > 2 & contain only letters
+     *
+     * @param name - the name
+     * @return true if valid name else false
+     */
+    public static boolean isValidName(String name) {
+        String regex1 = "^[a-zA-Z]+$";
+        Pattern pattern = Pattern.compile(regex1);
+
+        return name != null && pattern.matcher(name).matches();
+    }
+
+    /**
+     * Is valid password : check if The length of the password > 6 & At least one capital letter
+     *
+     * @param password - the password
+     * @return true if valid password else false
+     */
+    public static boolean isValidPassword(String password) {
+        logger.debug("Check valid password");
+        //&& password.matches(".[A-Z].")
+        return password != null  && password.length() >= 6;
+    }
+
+    /**
+     * Is valid email: check if syntax of email is valid
+     *
+     * @param emailAddress - the user email
+     * @return true if valid emailAddress else false
+     */
+    public static boolean isValidEmail(String emailAddress) {
+        logger.debug("Check valid email");
+        String regexPattern = "^(.+)@(\\S+)$";
+        return emailAddress != null && Pattern.compile(regexPattern).matcher(emailAddress).matches();
+    }
+
+
+    public static String getPasswordConstraints() {
+        return "password length must be at least 6 characters long and contain one capital letter";
+    }
+
+
+    public static String getNameConstraints() {
+        return " must contains at least 2 alphabetical letters and contain only characters";
+    }
+
+}
